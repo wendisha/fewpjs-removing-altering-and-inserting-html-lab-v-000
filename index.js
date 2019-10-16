35be90937a949e3032fe3760c68424a5590afe72
Wendy Calderon <wendycalderon@gmail.com>
2019-10-16 17:00:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Write your code here!
-let element = document.getElementById('main').remove()
-// element.remove();
+document.getElementById('main').remove()
 
 let newHeader = document.createElement('h1');
 newHeader.id = 'victory'
 newHeader.innerHTML = 'is the champion'
 document.body.appendChild(newHeader);
-

71094cf0828f742aed07c88a7ba34bfbbdf385ce
Wendy Calderon <wendycalderon@gmail.com>
2019-10-16 16:58:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,13 +7,3 @@ newHeader.id = 'victory'
 newHeader.innerHTML = 'is the champion'
 document.body.appendChild(newHeader);
 
-
-
-
-document.querySelector('main').remove()
-
-const newHeader = document.createElement('h1')
-
-newHeader.id = 'victory'
-
-newHeader.innerHTML = "YOUR-NAME is the champion"

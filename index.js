5b671b1920448713a897bde69c6f51230606cc35
Wendy Calderon <wendycalderon@gmail.com>
2019-10-16 16:58:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 // Write your code here!
-let element = document.getElementById('main')
-element.remove();
+let element = document.getElementById('main').remove()
+// element.remove();
 
 let newHeader = document.createElement('h1');
 newHeader.id = 'victory'
 newHeader.innerHTML = 'is the champion'
 document.body.appendChild(newHeader);
+
+
+
+
+document.querySelector('main').remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "YOUR-NAME is the champion"

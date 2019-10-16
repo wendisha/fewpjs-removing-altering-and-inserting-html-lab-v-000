5c291d9c8d7b72d6aa754e1b55f2a591766cfa41
Wendy Calderon <wendycalderon@gmail.com>
2019-10-16 17:01:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove()
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.innerHTML = 'is the champion'
+document.body.appendChild(newHeader);

0da7d5fc964e8e4efdff5f5f352a0d55cff452f1
Wendy Calderon <wendycalderon@gmail.com>
2019-10-16 16:57:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 // Write your code here!
-document.body.appendChild(element)
-let ul = document.createElement('ul')
- 
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li')
-  li.innerHTML = (i + 1).toString()
-  ul.appendChild(li)
-}
- 
-element.appendChild(ul)
+let element = document.getElementById('main')
+element.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.innerHTML = 'is the champion'
+document.body.appendChild(newHeader);
